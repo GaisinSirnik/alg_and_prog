29fa9c13e43d97f34c96b9b134b4fafb79b1f861
GaisinSirnik <148848596+GaisinSirnik@users.noreply.github.com>
2023-10-25 13:56:39 +0300

    Add files via upload

---
+++ Contest03/05/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+int main() {
+
+	std::map<std::string, int> counts;
+	int a;
+	std ::cin >>  a;
+	std::string s;
+	int maxCount = 0;
+
+	for (int i = 0; i < a; i++) {
+		std::cin >> s;
+		counts[s]++;
+
+		if (counts[s] > maxCount)
+			maxCount = counts[s];
+
+	}
+	for (auto& entry : counts) {
+		if (entry.second == maxCount)
+			std::cout << entry.first << " ";
+	}
+	return 0;
+
+}
